71abf55bfcee74bdfb9d0e238bcbcd38b40205d4
deepan <deepanappu1999@gmail.com>
2024-08-28 21:38:15 +0530

    adding route show movie details and adding cusor pointer

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,24 +1,22 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { imagesBaseUrl, MoviesService } from '../../services/movies.service';
 import { Movie } from '../../models/movies';
 import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe,RouterModule],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
-export class MovieComponent {
-
-   
-  private  moviesService  =  inject(MoviesService);
+export class MovieComponent implements OnInit{
+ 
   public imagesBaseUrl = imagesBaseUrl;
   @Input() movie!: Movie;
 
   ngOnInit(): void {
-    console.log(this.movie)
   }
 
 }
--- src/app/components/pages/show-movie/show-movie.component.ts
+++ src/app/components/pages/show-movie/show-movie.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { imagesBaseUrl, MoviesService } from '../../../services/movies.service';
+import { Movie } from '../../../models/movies';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-movie',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './show-movie.component.html',
   styleUrl: './show-movie.component.css'
 })
-export class ShowMovieComponent {
+export class ShowMovieComponent implements OnInit {
+  
+  private moiveServices = inject(MoviesService);
+  movie!:Movie;
+  movieId!:number
+  public imagesBaseUrl = imagesBaseUrl;
 
+  constructor(private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.movieId = params['movieId']
+      console.log(this.movieId);
+    })
+    this.moiveServices.fetchMovieById(this.movieId).subscribe((res:any)=> {
+      this.movie = res
+      console.log(this.movie.poster_path)
+    })
+  }
 }
